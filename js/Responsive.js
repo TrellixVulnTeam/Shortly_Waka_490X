c54e43363fdfbff056149da722f5a067c9067beb
Waddimi Saint Louis <94648767+waka-code@users.noreply.github.com>
2022-05-10 01:21:04 +0800

    actual

---
+++ js/Responsive.js
@@ -0,0 +1,14 @@
+
+let menu = document.getElementById('menu');
+let nav = document.querySelector('#nav');
+
+menu.addEventListener("click", () =>{
+    nav.classList.toggle("NAV");
+});
+
+/*MenuHamburguesa remover
+document.getElementById("menus").onclick = remover;
+function remover(){
+    navegarnavHamburguesa.classList.toggle("menunavHamburguesa");
+}*/
+
---
+++ js/index.js
@@ -0,0 +1,58 @@
+window.onload = inicio;
+
+const btn = document.getElementById(`btn_Sorten`);
+const linkShorten = document.getElementById(`link_shorten`);
+const inputShorten = document.getElementById(`input_shorten`);
+const errShorten = document.getElementById(`err_shorten`);
+
+function inicio(){
+   btn.addEventListener('click',shorten);
+
+}
+
+function shorten(e){
+    e.preventDefault();
+    if(inputShorten.value === ``){
+       errShorten.value;
+       errShorten.textContent = 'Please, Complete field.';
+       errShorten.style.color = 'red';
+       
+    }
+    else{
+
+    const text = inputShorten.value;
+    const li = document.createElement('li');
+    const p = document.createElement('p');
+     p.textContent = text;
+
+    li.appendChild(p);
+    li.appendChild(addDelete());
+    linkShorten.appendChild(li);
+
+    linkShorten.style.display=`block`;
+    li.style.display=`flex`;
+    li.style.justifyContent=`space-between`;
+    li.style.alignItems=`center`;
+    p.style.cursor= `pointer`
+    
+    inputShorten.value=``;
+}
+}
+
+function addDelete(){
+    const deleteBtn = document.createElement(`button`);
+
+    deleteBtn.textContent = `X`;
+    deleteBtn.style.backgroundColor = `red`;
+    deleteBtn.style.border = `none`;
+    deleteBtn.style.padding = `2px 8px`;
+    deleteBtn.style.borderRadius = `100px`;
+    deleteBtn.className = `btn-delete`
+
+    deleteBtn.addEventListener('click',(e) =>{
+        const item = e.target.parentElement;
+        linkShorten.removeChild(item);
+    });
+    return deleteBtn;
+}
+
