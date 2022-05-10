3f2f73943f3a0497dfd34fa60a88b3b062151129
Waddimi Saint Louis <94648767+waka-code@users.noreply.github.com>
2022-05-11 03:44:53 +0800

    actual

--- js/index.js
+++ js/index.js
@@ -4,13 +4,17 @@ const btn = document.getElementById(`btn_Sorten`);
 const linkShorten = document.getElementById(`link_shorten`);
 const inputShorten = document.getElementById(`input_shorten`);
 const errShorten = document.getElementById(`err_shorten`);
+const Api = {
+              key:`9551a0c48987ee4ffadb06dcc3f46ac24ebe9`,
+              url:`https://cutt.ly/api/api.php`
+               }
 
 function inicio(){
    btn.addEventListener('click',shorten);
 
 }
 
-function shorten(e){
+async function shorten(e){
     e.preventDefault();
     if(inputShorten.value === ``){
        errShorten.value;
@@ -20,25 +24,32 @@ function shorten(e){
        
     }
     else{
-
-    const text = inputShorten.value;
-    const li = document.createElement('li');
-    const p = document.createElement('p');
-     p.textContent = text;
-
-    li.appendChild(p);
-    li.appendChild(addDelete());
-    linkShorten.appendChild(li);
-
-    linkShorten.style.display=`block`;
-    li.style.display=`flex`;
-    li.style.justifyContent=`space-between`;
-    li.style.alignItems=`center`;
-    p.style.cursor= `pointer`
+      try{
+        const apirest = await fetch(`?q=${Api.url}&appid=${Api.key}`);
+        const data = await apirest.json();
+        console.log(data)    
+        const text = inputShorten.value;
+        const li = document.createElement('li');
+        const p = document.createElement('p');
+         p.textContent = text;
     
-    inputShorten.value=``;
-    errShorten.style.visibility="hidden"
-}
+        li.appendChild(p);
+        li.appendChild(addDelete());
+        linkShorten.appendChild(li);
+    
+        linkShorten.style.display=`block`;
+        li.style.display=`flex`;
+        li.style.justifyContent=`space-between`;
+        li.style.alignItems=`center`;
+        p.style.cursor= `pointer`
+        
+        inputShorten.value=``;
+        errShorten.style.visibility="hidden"
+      }
+      catch(err){
+        console.log(err)
+    }
+} 
 }
 
 function addDelete(){
@@ -52,10 +63,15 @@ function addDelete(){
     deleteBtn.className = `btn-delete`
 
     deleteBtn.addEventListener('click',(e) =>{
-
         const item = e.target.parentElement;
         linkShorten.removeChild(item);
-        linkShorten.remove()
+
+        for(let i = 0; i=item.length; i++) {
+            if(item[i].pop() === ``){
+                linkShorten.appendChild(item[i]);
+            }
+        }
+        
 
     });
     return deleteBtn;
